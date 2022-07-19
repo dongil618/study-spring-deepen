0ab77d79deb1abe73dc36afe1ed522df0459756d
dongil618 <rlaehddlf618@gmail.com>
2022-07-19 16:38:28 +0900

    test폴더에 integration폴더 생성 후 이동

--- src/test/java/com/hanghae99/studyspringdeepn2/integration/FoodIntegrationTest.java
+++ src/test/java/com/hanghae99/studyspringdeepn2/integration/FoodIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.hanghae99.studyspringdeepn2;
+package com.hanghae99.studyspringdeepn2.integration;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
