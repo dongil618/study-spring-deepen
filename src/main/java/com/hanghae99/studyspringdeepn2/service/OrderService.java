d6b8dc2a00f0d5894a28032acc15dc5cf022032b
dongil618 <rlaehddlf618@gmail.com>
2022-07-21 11:19:21 +0900

    OrderService 리팩토링

--- src/main/java/com/hanghae99/studyspringdeepn2/service/OrderService.java
+++ src/main/java/com/hanghae99/studyspringdeepn2/service/OrderService.java
@@ -27,53 +27,25 @@ public class OrderService {
     private final FoodRepository foodRepository;
 
     public OrderResponseDto orderRequest(OrderRequestDto orderRequestDto) {
-        Long restaurantId = orderRequestDto.getRestaurantId();
+        // Restaurant ID를 통해 음식점 정보 가져오기
+        Restaurant restaurant = restaurantRepository.findById(orderRequestDto.getRestaurantId())
+                .orElseThrow(() -> new IllegalArgumentException("음식점 정보를 찾을 수 없습니다."));
+
         List<OrderFoodsRequestDto> foods = orderRequestDto.getFoods();
-        Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
-        Long totalPrice = Long.valueOf(restaurant.getDeliveryFee());
-        for (OrderFoodsRequestDto orderFoodsRequestDto : foods) {
-            Long foodId = orderFoodsRequestDto.getId();
-            Integer foodPrice = foodRepository.findById(foodId).get().getPrice();
-            Integer foodQuantity = orderFoodsRequestDto.getQuantity();
-            if(foodQuantity < 1){
-                throw new IllegalArgumentException("허용된 값이 아닙니다.");
-            }
-            if(foodQuantity > 100){
-                throw new IllegalArgumentException("허용된 값이 아닙니다.");
-            }
-//            System.out.println("foodPrice : " + foodPrice + " foodQuantity : " + foodQuantity);
-            totalPrice += Long.valueOf(foodPrice) * Long.valueOf(foodQuantity);
-        }
-        // 주문 음식가격의 총합 = 최종 결제금액 - 배달비용
-        if(restaurant.getMinOrderPrice() > totalPrice - restaurant.getDeliveryFee()){
-            throw new IllegalArgumentException("주문 음식 가격의 총 합이 최소 주문 금액을 넘지 못했습니다.");
-        }
-//        System.out.println("----------------------------");
-//        System.out.println("totalPrice : " + totalPrice);
+
+        Long totalPrice = getTotalPrice(foods,restaurant);
+
+        minOrderPriceValidation(restaurant, totalPrice);
+
         Order order = new Order(restaurant, totalPrice);
+
+        // 저장 후에 orderId 값 가져오기(OrderDetail 테이블 저장을 위함)
         Long orderId = orderRepository.save(order).getId();
-//        System.out.println("orderId : " + orderId);
-        for (int i = 0; i < foods.size(); i++) {
-            Long foodId = foods.get(i).getId();
-            Food food = foodRepository.findById(foodId).get();
-            Order orderInOrderDetail = orderRepository.findById(orderId).get();
-            Integer quantity = foods.get(i).getQuantity();
-            OrderDetail orderDetail = new OrderDetail(food, orderInOrderDetail, quantity);
-            orderDetailRepository.save(orderDetail);
-        }
-        List<OrderDetail> orderDetailList = orderDetailRepository.findAllByOrder(order);
-//        System.out.print("orderDetailList 크기 : " + orderDetailList.size());\
-        List<OrderFoodsResponseDto> foodList = new ArrayList<>();
-        for (OrderDetail od: orderDetailList) {
-            OrderFoodsResponseDto orderFoodsResponseDto = new OrderFoodsResponseDto(od);
-//            System.out.println(orderFoodsResponseDto);
-            foodList.add(orderFoodsResponseDto);
-//            System.out.println(od.getFood().getName());
-//            System.out.println(od.getFood().getPrice());
-//            System.out.println(od.getQuantity());
-        }
-        //        System.out.println(orderResponseDto);
-        // System.out.println("orderRequestDto = " + orderRequestDto.toString());
+        saveOrderDetail(foods, orderId);
+
+        // response로 넘겨줄 foods 배열 만들기
+        List<OrderFoodsResponseDto> foodList = getFoodList(order);
+
         return new OrderResponseDto(restaurant,foodList, order);
     }
 
@@ -82,7 +54,8 @@ public class OrderService {
         List<Order> orders = orderRepository.findAll();
         for(Order order:orders){
             List<OrderFoodsResponseDto> foodList = new ArrayList<>();
-            Restaurant restaurant = restaurantRepository.findById(order.getRestaurant().getId()).get();
+            Restaurant restaurant = restaurantRepository.findById(order.getRestaurant().getId())
+                    .orElseThrow(() -> new IllegalArgumentException("음식점 정보가 없습니다."));
             List<OrderDetail> orderDetailList = orderDetailRepository.findAllByOrder(order);
             for (OrderDetail od: orderDetailList) {
                 OrderFoodsResponseDto orderFoodsResponseDto = new OrderFoodsResponseDto(od);
@@ -91,5 +64,63 @@ public class OrderService {
             successOrderList.add(new OrderResponseDto(restaurant,foodList, order));
         }
         return successOrderList;
+    }
+
+    public void foodQuantityValidation(Integer foodQuantity){
+        if(foodQuantity < 1){
+            throw new IllegalArgumentException("허용된 값이 아닙니다.");
+        }
+        if(foodQuantity > 100){
+            throw new IllegalArgumentException("허용된 값이 아닙니다.");
+        }
+    }
+
+    public void minOrderPriceValidation(Restaurant restaurant, Long totalPrice){
+        // 주문 음식가격의 총합 = 최종 결제금액 - 배달비용
+        if(restaurant.getMinOrderPrice() > totalPrice - restaurant.getDeliveryFee()){
+            throw new IllegalArgumentException("주문 음식 가격의 총 합이 최소 주문 금액을 넘지 못했습니다.");
+        }
+    }
+
+    public Long getTotalPrice(List<OrderFoodsRequestDto> foods, Restaurant restaurant){
+        Long totalPrice = 0L;
+
+        for (OrderFoodsRequestDto orderFoodsRequestDto : foods) {
+            Long foodId = orderFoodsRequestDto.getId();
+            Integer foodPrice = foodRepository.findById(foodId)
+                    .orElseThrow(() -> new IllegalArgumentException("음식정보를 찾을 수 없습니다."))
+                    .getPrice();
+            Integer foodQuantity = orderFoodsRequestDto.getQuantity();
+            foodQuantityValidation(foodQuantity);
+            totalPrice += Long.valueOf(foodPrice) * Long.valueOf(foodQuantity);
+        }
+
+        // 마지막에 배달비까지 더하기
+        totalPrice += Long.valueOf(restaurant.getDeliveryFee());
+        return  totalPrice;
+    }
+
+    public void saveOrderDetail(List<OrderFoodsRequestDto> foods, Long orderId){
+        // 주문 저장하기
+        for (OrderFoodsRequestDto orderFoodsRequestDto : foods) {
+            Long foodId = orderFoodsRequestDto.getId();
+            Food food = foodRepository.findById(foodId)
+                    .orElseThrow(() -> new IllegalArgumentException("음식점보를 찾을 수 없습니다."));
+            Order orderInOrderDetail = orderRepository.findById(orderId)
+                    .orElseThrow(() -> new IllegalArgumentException("주문정보를 찾을 수 없습니다."));
+            Integer quantity = orderFoodsRequestDto.getQuantity();
+            OrderDetail orderDetail = new OrderDetail(food, orderInOrderDetail, quantity);
+            orderDetailRepository.save(orderDetail);
+        }
+    }
+    public List<OrderFoodsResponseDto> getFoodList(Order order){
+        List<OrderFoodsResponseDto> foodList = new ArrayList<>();
+        // 주문 객체를 통해 해당 주문의 주문 상세 내역 리스트 불러오기
+        List<OrderDetail> orderDetailList = orderDetailRepository.findAllByOrder(order);
+        for (OrderDetail od: orderDetailList) {
+            OrderFoodsResponseDto orderFoodsResponseDto = new OrderFoodsResponseDto(od);
+            foodList.add(orderFoodsResponseDto);
+        }
+        return foodList;
     }
 }
